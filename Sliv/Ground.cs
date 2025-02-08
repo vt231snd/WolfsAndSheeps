2d122502f82ba2876efc27e4dc93c983b62b0243
vt231snd <vt231_snd@student.ztu.edu.ua>
2025-02-08 14:13:13 +0200

    Removed unused using

--- Sliv/Ground.cs
+++ Sliv/Ground.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Reflection.Emit;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
-using System.Drawing.Printing;
-using static System.Windows.Forms.AxHost;
-using System.Net.NetworkInformation;
 
 namespace Sliv
 {

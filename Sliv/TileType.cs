4624cf43c40de05228f5d5fbf4792d63d9d0e7ee
vt231snd <vt231_snd@student.ztu.edu.ua>
2025-02-08 16:31:21 +0200

    Added new ToleType enum

---
+++ Sliv/TileType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sliv
+{
+    public enum TileType
+    {
+        Empty = 0,
+        Wall = 1,
+        Target = 2,
+        Wolf = 3
+    }
+}

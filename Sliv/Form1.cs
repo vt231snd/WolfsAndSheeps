c993de5b7a4238890d3db8ef7d393017c5893029
vt231snd <vt231_snd@student.ztu.edu.ua>
2025-02-08 16:10:18 +0200

    Divided some methods and renamed

--- Sliv/Form1.cs
+++ Sliv/Form1.cs
@@ -119,33 +119,34 @@ namespace Sliv
             b2.Enabled = false;
             b3.Enabled = false;
         }
-        private void panel3Vis()
-        {
-            b2.Enabled = false;
-            b3.Enabled = false;
-        }
         private void Exit_Click(object sender, EventArgs e)
         {
             Close();
         }
 
-        private void lvlsEn(char lvls)
+        private void EnableLevels(string completedLevels)
         {
-            if (Char.GetNumericValue(lvls) == 1) { b2.Enabled = true; b1.BackColor = Color.Green; }
-            else if (Char.GetNumericValue(lvls) == 2) { b3.Enabled = true; b2.BackColor = Color.Green; }
-            else if (Char.GetNumericValue(lvls) == 3)
+            if (completedLevels.Contains('1'))
+            {
+                b2.Enabled = true;
+                b1.BackColor = Color.Green;
+            }
+            if (completedLevels.Contains('2'))
+            {
+                b3.Enabled = true;
+                b2.BackColor = Color.Green;
+            }
+            if (completedLevels.Contains('3'))
             {
                 b3.BackColor = Color.Green;
-                if(_formLanguage == "Ua") {
-                    MessageBox.Show("Вы пройшли всі рівні. Чекайте оновлень)");
-                } else if(_formLanguage == "Eng")
-                {
-                    MessageBox.Show("You complete all levels. Wait for updates)");
-                }
+                string message = _formLanguage == "Ua"
+                    ? "Ви пройшли всі рівні. Чекайте оновлень :)"
+                    : "You've completed all levels. Stay tuned for updates :)";
+                MessageBox.Show(message);
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void PlayButton_Click(object sender, EventArgs e)
         {
             _currentPage++;
             panel3.Visible = true;
@@ -153,18 +154,11 @@ namespace Sliv
             button1.BringToFront();
             button1.Visible = true;
 
-            if (_levels.ReadLog() != 0)
-            {
-                string s = _levels.ReadLog().ToString();
-                char[] lvls = s.ToCharArray();
-                for (int i = 0; i < lvls.Length; i++)
-                {
-                    lvlsEn(lvls[i]);
-                }
-            }
+            string levelsCompleted = _levels.ReadLog().ToString() ?? string.Empty;
+            EnableLevels(levelsCompleted);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void BackButton_Click(object sender, EventArgs e)
         {
             _currentPage--;
             if (_currentPage == 0)
@@ -175,79 +169,21 @@ namespace Sliv
             }
             else if (_currentPage == 1)
             {
-                if (_levels.ReadLog() == 0) _levels.UpdateLog(_ground.CompleteLevelInstance._level.ToString());
-                else _levels.UpdateLog(_levels.ReadLog().ToString() + _ground.CompleteLevelInstance._level.ToString());
-                string s = _levels.ReadLog().ToString();
-                char[] lvls = s.ToCharArray();
-
-                for (int i = 0; i < lvls.Length; i++)
+                string currentLevel = _ground?.CompleteLevelInstance?._level.ToString();
+                if (!string.IsNullOrEmpty(currentLevel))
                 {
-                    lvlsEn(lvls[i]);
+                    _levels.UpdateLog(_levels.ReadLog() + currentLevel);
                 }
+
+                string levelsCompleted = _levels.ReadLog().ToString() ?? string.Empty;
+                EnableLevels(levelsCompleted);
+
                 panel1.Controls.Remove(_ground);
                 panel3.Visible = true;
                 panel2.Visible = false;
                 button1.BringToFront();
                 button1.Visible = true;
             }
-
-        }
-
-        private void b1_Click(object sender, EventArgs e)
-        {
-            _currentPage++;
-            Button btn = (Button)sender;
-            switch (btn.Name)
-            {
-                case "b1": _ground = new Ground(1, 4, 2, 5, 4, 6, _formLanguage); break;
-                case "b2": _ground = new Ground(2, 3, 3, 5, 4, 6, _formLanguage); break;
-                case "b3": _ground = new Ground(3, 2, 2, 3, 6, 6, _formLanguage); break;
-            }
-
-            panel1.Controls.Add(_ground);
-            _ground.Dock = DockStyle.Fill;
-            _ground.BringToFront();
-            button1.BringToFront();
-            button1.Visible = true;
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-        }
-
-
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-            if (radioButton1.Checked)
-            {
-                groupBox1.Text = "Мова";
-                radioButton1.Text = "Українська";
-                radioButton2.Text = "Англійська";
-
-                label1.Text = "Рівні";
-                button2.Text = "Грати";
-                Exit.Text = "Вихід";
-
-                radioButton2.BackColor = SystemColors.Control;
-                radioButton1.BackColor = Color.Green;
-                _formLanguage = "Ua";
-            }
-            else if (radioButton2.Checked)
-            {
-                groupBox1.Text = "Language";
-                radioButton1.Text = "Ukrainian";
-                radioButton2.Text = "English";
-
-                label1.Text = "Levels";
-                button2.Text = "Play";
-                Exit.Text = "Exit";
-
-                radioButton1.BackColor = SystemColors.Control;
-                radioButton2.BackColor = Color.Green;
-                _formLanguage = "Eng";
-            }
-            _language = new Lang(_formLanguage);
         }
     }
 }
